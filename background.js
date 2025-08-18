45b27bbc8ad9043f02be27da62734fb210d1d4b6
AudeizReading <alellouc@student.42nice.fr>
2025-08-18 15:54:51 +0200

    Add i18n, side panel, context menu, and new filters
    Introduces internationalization (i18n) with English and French locales, replaces popup with a Chrome side panel, adds a context menu for quick access, and implements intensity sliders for each color vision deficiency and blur simulation. Updates UI for accessibility and error handling, removes old screenshots, and adds new feature images. Manifest updated for default locale and permissions.

--- background.js
+++ background.js
@@ -1,10 +1,34 @@
 // Only added for complying with the manifest v3 requirements
 // This file is not used in the extension but is required for the manifest v3
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Color Perception Simulator installed.");
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log(
+    "Extension updated to version:",
+    chrome.runtime.getManifest().version
+  );
+  if (details.reason === "update") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (!tab || !tab.url) return;
+      const url = tab.url;
+
+      // On évite les chrome:// etc
+      if (
+        url.startsWith("chrome://") ||
+        url.startsWith("chrome-extension://") ||
+        url.startsWith("https://chrome.google.com/webstore")
+      ) {
+        console.error(
+          chrome.i18n.getMessage("error_extension_usage_title"),
+          chrome.i18n.getMessage("error_extension_usage_message")
+        );
+        return;
+      }
+    });
+  }
+
   chrome.contextMenus.create({
     id: "openSidePanel",
-    title: "Open in side panel",
+    title: chrome.i18n.getMessage("context_menu_title"),
     contexts: ["all"],
   });
   chrome.sidePanel
@@ -16,5 +40,21 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "openSidePanel") {
     // This will open the panel in all the pages on the current window.
     chrome.sidePanel.open({ windowId: tab.windowId });
+  }
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (tab.url) {
+    if (
+      tab.url.startsWith("chrome://") ||
+      tab.url.startsWith("chrome-extension://") ||
+      tab.url.startsWith("chrome.google.com/webstore")
+    ) {
+      // Désactiver le side panel pour cet onglet
+      chrome.sidePanel.setOptions({ tabId, enabled: false });
+    } else {
+      // Réactiver sur les autres sites
+      chrome.sidePanel.setOptions({ tabId, enabled: true });
+    }
   }
 });
