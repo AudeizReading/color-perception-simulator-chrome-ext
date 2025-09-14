f75b6b3e8bdc9653c40f298aefbe0bd28ed2425c
AudeizReading <alellouc@student.42nice.fr>
2025-09-14 09:03:51 +0200

    add border-color to active sections (those who were clicked by user)

--- side_panel.js
+++ side_panel.js
@@ -1,91 +1,91 @@
 function applyI18n() {
-  for (const el of document.querySelectorAll("[data-i18n]")) {
-    el.prepend(
-      document.createTextNode(chrome.i18n.getMessage(el.dataset.i18n))
-    );
-  }
-  for (const el of document.querySelectorAll("[data-i18n-aria-label]")) {
-    el.setAttribute(
-      "aria-label",
-      chrome.i18n.getMessage(el.dataset.i18nAriaLabel)
-    );
-  }
-  for (const el of document.querySelectorAll("[data-i18n-placeholder]")) {
-    el.setAttribute(
-      "placeholder",
-      chrome.i18n.getMessage(el.dataset.i18nPlaceholder)
-    );
-  }
+	for (const el of document.querySelectorAll("[data-i18n]")) {
+		el.prepend(
+			document.createTextNode(chrome.i18n.getMessage(el.dataset.i18n))
+		);
+	}
+	for (const el of document.querySelectorAll("[data-i18n-aria-label]")) {
+		el.setAttribute(
+			"aria-label",
+			chrome.i18n.getMessage(el.dataset.i18nAriaLabel)
+		);
+	}
+	for (const el of document.querySelectorAll("[data-i18n-placeholder]")) {
+		el.setAttribute(
+			"placeholder",
+			chrome.i18n.getMessage(el.dataset.i18nPlaceholder)
+		);
+	}
 }
 
 function onClickSimulateButton(filter) {
-  let rangeValue = "1.0";
+	let rangeValue = "1.0";
 
-  if (filter !== "reset") {
-    rangeValue = document.getElementById(`range-${filter}`).value;
-  } else {
-    let blurDefaultValue = "0.3";
-    ["protanope", "deuteranope", "tritanope", "achromatope", "blur"].forEach(
-      (f) => {
-        if (f === "blur") {
-          document.getElementById(`range-${f}`).value = blurDefaultValue;
-        } else {
-          document.getElementById(`range-${f}`).value = rangeValue;
-        }
-      }
-    );
-  }
-  applyFilter(filter, rangeValue);
+	if (filter !== "reset") {
+		rangeValue = document.getElementById(`range-${filter}`).value;
+	} else {
+		let blurDefaultValue = "0.3";
+		["protanope", "deuteranope", "tritanope", "achromatope", "blur"].forEach(
+			(f) => {
+				if (f === "blur") {
+					document.getElementById(`range-${f}`).value = blurDefaultValue;
+				} else {
+					document.getElementById(`range-${f}`).value = rangeValue;
+				}
+			}
+		);
+	}
+	applyFilter(filter, rangeValue);
 }
 
 function onClickTroubleDefinitionChevron(el, event) {
-  const rect = el.getBoundingClientRect();
-  const styles = getComputedStyle(el);
+	const rect = el.getBoundingClientRect();
+	const styles = getComputedStyle(el);
 
-  const em = parseFloat(styles.fontSize) || 16;
-  const rem =
-    parseFloat(getComputedStyle(document.documentElement).fontSize) || 16;
+	const em = parseFloat(styles.fontSize) || 16;
+	const rem =
+		parseFloat(getComputedStyle(document.documentElement).fontSize) || 16;
 
-  const chevronWidth = 0.6 * em;
-  const rightOffset = 0.25 * rem;
+	const chevronWidth = 0.6 * em;
+	const rightOffset = 0.25 * rem;
 
-  // petite tolérance pour faciliter le clic
-  const tolerance = 8; // px
+	// petite tolérance pour faciliter le clic
+	const tolerance = 8; // px
 
-  // distance du clic au bord droit du h2
-  const fromRight = rect.right - event.clientX;
+	// distance du clic au bord droit du h2
+	const fromRight = rect.right - event.clientX;
 
-  const hotspot = chevronWidth + rightOffset + tolerance;
+	const hotspot = chevronWidth + rightOffset + tolerance;
 
-  if (fromRight <= hotspot) {
-    el.parentElement.classList.toggle("is-open");
-  }
+	if (fromRight <= hotspot) {
+		el.parentElement.classList.toggle("is-open");
+	}
 }
 
 function createAlertError() {
-  const alert = document.createElement("div");
-  const alertTitle = document.createElement("h2");
-  alertTitle.prepend(
-    document.createTextNode(
-      chrome.i18n.getMessage("error_extension_usage_title")
-    )
-  );
+	const alert = document.createElement("div");
+	const alertTitle = document.createElement("h2");
+	alertTitle.prepend(
+		document.createTextNode(
+			chrome.i18n.getMessage("error_extension_usage_title")
+		)
+	);
 
-  const alertMessageBody = document.createElement("p");
-  alertMessageBody.prepend(
-    document.createTextNode(
-      chrome.i18n.getMessage("error_extension_usage_message")
-    )
-  );
+	const alertMessageBody = document.createElement("p");
+	alertMessageBody.prepend(
+		document.createTextNode(
+			chrome.i18n.getMessage("error_extension_usage_message")
+		)
+	);
 
-  alert.appendChild(alertTitle);
-  alert.appendChild(alertMessageBody);
-  alert.classList.add("alert-error");
+	alert.appendChild(alertTitle);
+	alert.appendChild(alertMessageBody);
+	alert.classList.add("alert-error");
 
-  document.body.prepend(alert);
-  setTimeout(() => {
-    alert.remove();
-  }, 8000);
+	document.body.prepend(alert);
+	setTimeout(() => {
+		alert.remove();
+	}, 8000);
 }
 
 /**
@@ -101,57 +101,70 @@ function createAlertError() {
 const filtersStack = [];
 
 function applyFilter(filter, severity = "1.0") {
-  chrome.tabs.query(
-    {
-      active: true,
-      currentWindow: true,
-    },
-    (tabs) => {
-      const url = tabs[0]?.url || "";
-      if (isBlockedUrl(url)) {
-        createAlertError();
-      } else {
-        const cssFilter = getMatrixFilter(filter, severity);
-        const last = filtersStack.pop();
-        let perceptionFilter;
+	chrome.tabs.query(
+		{
+			active: true,
+			currentWindow: true
+		},
+		(tabs) => {
+			const url = tabs[0]?.url || "";
+			if (isBlockedUrl(url)) {
+				createAlertError();
+			} else {
+				const cssFilter = getMatrixFilter(filter, severity);
+				const last = filtersStack.pop();
+				let perceptionFilter;
 
-        if (last) {
-          removeCSSFilter(last, tabs[0].id);
-        }
-        if (filter === "reset" && filtersStack.length === 0) {
-          perceptionFilter = `html { filter: none; }`;
-        } else if (filter === "blur") {
-          perceptionFilter = createBlurFilter(cssFilter);
-        } else {
-          perceptionFilter = createfeColorMatrixFilter(filter, cssFilter);
-        }
+				if (last) {
+					removeCSSFilter(last, tabs[0].id);
+				}
+				if (filter === "reset" && filtersStack.length === 0) {
+					perceptionFilter = `html { filter: none; }`;
+				} else if (filter === "blur") {
+					perceptionFilter = createBlurFilter(cssFilter);
+				} else {
+					perceptionFilter = createfeColorMatrixFilter(filter, cssFilter);
+				}
 
-        if (perceptionFilter) {
-          filtersStack.push(perceptionFilter);
-          insertCSSFilter(perceptionFilter, tabs[0].id);
-        }
-      }
-    }
-  );
+				if (perceptionFilter) {
+					filtersStack.push(perceptionFilter);
+					insertCSSFilter(perceptionFilter, tabs[0].id);
+				}
+			}
+		}
+	);
 }
 
 /* Debut actions du script */
+
 // Apply the filter when the user clicks on the filter button
+function toggleActiveSections(filter, el) {
+	const oldActives = document.querySelectorAll("section.card.active");
+	oldActives?.forEach((el) => {
+		el?.classList.toggle("active");
+	});
+	if (filter !== "reset") {
+		el?.parentElement?.parentElement?.classList.toggle("active");
+	}
+}
+
 // and when the user changes the range input
 filters.forEach((filter) => {
-  document.getElementById(filter).addEventListener("click", () => {
-    onClickSimulateButton(filter);
-  });
+	const el = document.getElementById(filter);
+	el.addEventListener("click", () => {
+		toggleActiveSections(filter, el);
+		onClickSimulateButton(filter);
+	});
 
-  if (filter !== "reset") {
-    document
-      .getElementById(`range-${filter}`)
-      .addEventListener("input", (event) => {
-        const value = event.target.value;
+	if (filter !== "reset") {
+		document
+			.getElementById(`range-${filter}`)
+			.addEventListener("input", (event) => {
+				const value = event.target.value;
 
-        applyFilter(filter, value);
-      });
-  }
+				applyFilter(filter, value);
+			});
+	}
 });
 
 // Apply auto translation to the side panel
@@ -159,7 +172,7 @@ applyI18n();
 
 // Event listener sur accordeon pour ouvrir/fermer les sections
 document.querySelectorAll(".card > h2").forEach((h2) => {
-  h2.addEventListener("click", (e) => {
-    onClickTroubleDefinitionChevron(h2, e);
-  });
+	h2.addEventListener("click", (e) => {
+		onClickTroubleDefinitionChevron(h2, e);
+	});
 });
